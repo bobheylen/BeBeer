0332de263b78889716e61bfde9bc5fe539b56b85
ANTOINEJVDM\antoi <antoine.janssensvandermaelen@kuleuven.be>
2022-05-02 18:07:20 +0200

    login and register page layout 2.0

--- app/src/main/java/be/kuleuven/bebeer/MainActivity.java
+++ app/src/main/java/be/kuleuven/bebeer/MainActivity.java
@@ -2,7 +2,9 @@ package be.kuleuven.bebeer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onBtnRegister(View caller)
+    {
+        Intent intent = new Intent(this, registrationActivity.class);
+        startActivity(intent);
+    }
+
+    
 }
